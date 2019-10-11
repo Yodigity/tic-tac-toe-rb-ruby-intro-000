c1d22ba3ec0124cc792a08aa73102cdc3bffbd7e
Aaron Gokani <aarongokani@hotmail.com>
2019-10-11 04:29:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,23 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+def input_to_index(entered_number)
+  input = entered_number.to_i
+  space = input - 1
+end
+
+def move(board_array, pos, char = "X")
+  board_array[pos] = char
+end
+
+
+
 def turn_count(board)
   counter = 0
   board.each do |space|
@@ -97,3 +117,9 @@ def winner(board)
     nil
   end
 end
+
+def play(board)
+  while !over?
+    
+    
+  end
